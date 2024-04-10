9ee894666c226aa9e2065728131ef8d01f220727
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-04-09 23:42:23 -0300

    Ex-completo

--- exrept01.js
+++ exrept01.js
@@ -1,16 +1,38 @@
-let ini = document.getElementById('txtcontI')
-let fim = document.getElementById('txtcontF')
-let pas = document.getElementById('textpass')
-res = document.getElementById('res')
-
-
-
 function contar(){
+    let ini =  document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let pas = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    let iniR = Number(txtcontI.value)
-    let fimR = Number(txtcontF)
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossivel contar!'
+    } else {
+        res.innerHTML = `Contando: <br>`
+        let i = Number(ini.value)
+        let f  = Number(fim.value)
+        let p = Number(pas.value)
 
-    for (ini = iniR; ini <= fimR; ini++){
-       console.log(ini) 
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1.')
+            p = 1
+        }
+
+        if (i < f){
+          for( let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+        }
+     
+
+        } else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
+
 }
+
+
+
